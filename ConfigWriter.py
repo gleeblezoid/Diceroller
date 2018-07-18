f6744dbb65fed0f3014f74112919fd05838ef208
Ursula Searle <usearle@pivotal.io>
2018-07-18 23:24:25 +0100

    Fleshing out config writer

--- ConfigWriter.py
+++ ConfigWriter.py
@@ -9,3 +9,8 @@ print("Welcome to the dice roller customizer. "
       "This works best for systems which use pools of dice.")
 rpgsystem = input("What RPG system are you using?")
 sides = int(input("How many sides do your dice have?"))
+explode = input("Do your dice explode? Type either Yes or No")
+
+# Error handling
+
+if explode !=
