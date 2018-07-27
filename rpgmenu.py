6b3635fe023533c88639845020b47a66cdef236c
Ursula Searle <usearle@pivotal.io>
2018-07-27 22:07:19 +0100

    Commit initial state for rpgmenu and rpglist

--- rpgmenu.py
+++ rpgmenu.py
@@ -1,2 +1,6 @@
-if menu_choice == '7c':
-      import 7thSea
+print("Welcome to the RPG menu: Choose your preferred option by typing the shortcode or number")
+print("q: Return to main menu")
+import rpglist
+menu_choice = input("Please enter your choice:")
+if menu_choice == "q":
+    import DiceRoller
