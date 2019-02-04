2462c70f2c91d5b561a592eda21908fdc0704bd7
“Ursula <usearle@pivotal.io>
2019-02-04 20:15:22 +0000

    Remove indent

--- FateRoller.py
+++ FateRoller.py
@@ -6,7 +6,7 @@ import random
 from pathlib import Path
 
 rollagain = "Y"
-while str.upper(rollagain)=="Y":
+while str.upper(rollagain) == "Y":
     try:
         sides = 6
 
@@ -14,7 +14,7 @@ while str.upper(rollagain)=="Y":
             print("Silly human")
             import FateRoller
 
-    # Dice roll resolution
+        # Dice roll resolution
         i = 0
         while i < (pool):
             roll = random.randint(1, sides)
@@ -22,12 +22,12 @@ while str.upper(rollagain)=="Y":
             if roll == (1 or 2):
                 print("[+]", end=",")
             elif roll == (3 or 4):
-                print("[-]",end=",")
+                print("[-]", end=",")
             else:
-                print("[ ]",end=",")
-        rollagain=input("Do you want to roll the same again? Enter Y or N: ")
-            if str.upper(rollagain)=="Y"
-                pool=input("How many dice do you want to roll")
+                print("[ ]", end=",")
+
+        rollagain = input("Do you want to roll the same again? Enter Y or N: ")
+
     except (NameError, TypeError, ValueError):
 
         print("Sorry, try something else!")
