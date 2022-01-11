6a5bed444ae7ff35daa57ca041eb19ce41f508b6
gleeblezoid <hello@gleeblezoid.com>
2022-01-11 20:26:45 +0000

    Fixup to setup and requirements

--- setup.py
+++ setup.py
@@ -1,32 +1,17 @@
 from setuptools import setup, find_packages
-from io import open
-from os import path
-import pathlib
 
-# The directory containing this file
-HERE = pathlib.Path(__file__).parent
-# The text of the README file
-README = (HERE / "README.md").read_text()
-# automatically captured required modules for install_requires in requirements.txt and as well as configure dependency links
-with open(path.join(HERE, "requirements.txt"), encoding="utf-8") as f:
-    all_reqs = f.read().split("\n")
-install_requires = [
-    x.strip()
-    for x in all_reqs
-    if ("git+" not in x) and (not x.startswith("#")) and (not x.startswith("-"))
-]
-dependency_links = [x.strip().replace("git+", "") for x in all_reqs if "git+" not in x]
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 setup(
     name="gleeblezoid-diceroller",
     description="A dice rolling terminal app made for RPGs",
     version="1.0",
     packages=find_packages(),  # list of all packages
-    install_requires=install_requires,
     python_requires=">=3.6",  # any python greater than 2.7
     author="Urse Searle",
     keyword="dice, rpg, gaming",
-    long_description=README,
+    long_description=long_description,
     long_description_content_type="text/markdown",
     license="BSD",
     url="https://github.com/gleeblezoid/Diceroller",
