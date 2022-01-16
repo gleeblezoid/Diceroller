d8fd6c38a67bf3cafcdd03792539b34163170210
Urse <gleeblezoid@gmail.com>
2022-01-16 21:46:49 +0000

    Update __init__.py

--- gzdice/__init__.py
+++ gzdice/__init__.py
@@ -1,6 +1,6 @@
 """A Python diceroller designed for tabletop roleplayers and running in terminal to make you feel like a wizard."""
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 
 from .menus import base_menus as m
